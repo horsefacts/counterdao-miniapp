1536628afc8045d352ada3e5cd97129b56fa98fe
horsefacts <webmaster@terminally.online>
2025-04-30 01:54:00 -0400

    remove base

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,12 +1,11 @@
 import { farcasterFrame } from "@farcaster/frame-wagmi-connector";
 import { http, createConfig } from "wagmi";
-import { base, mainnet } from "wagmi/chains";
+import { mainnet } from "wagmi/chains";
 
 export const config = createConfig({
-  chains: [base, mainnet],
+  chains: [mainnet],
   connectors: [farcasterFrame()],
   transports: {
-    [base.id]: http(),
     [mainnet.id]: http(),
   },
 });
